19d93592f4357d56328b0d5554250ec1d3daaf98
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2025-05-28 17:32:45 +0700

    Delete code/week-09/Week9D directory

--- code/week-09/Week9D/app/src/main/java/com/example/week9d/Article.kt
+++
@@ -1,7 +0,0 @@
-package com.example.week9d
-
-data class Article(
-    val id: Int,
-    val title: String,
-    val content: String
-)
--- code/week-09/Week9D/app/src/main/java/com/example/week9d/week9Application.kt
+++
@@ -1,9 +0,0 @@
-package com.example.week9d
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class week9Application: Application() {
-
-}
